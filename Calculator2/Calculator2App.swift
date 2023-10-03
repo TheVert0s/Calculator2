018cc2eed77cc7660ddfbe9a6c8e90f2e6bccc30
a <a>
2023-10-03 14:44:51 +0200

    Initial Commit

---
+++ Calculator2/Calculator2App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator2App.swift
+//  Calculator2
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Calculator2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
